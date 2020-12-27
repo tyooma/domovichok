438ec9ba7136f3a9601cf0015fea99cc81c23ae1
tyooma <flying.alien.godzilla@gmail.com>
2020-12-28 00:13:04 +0200

    header icons

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react'
 import { FlatList, SafeAreaView, Text, View } from 'react-native'
-import { Icon, Divider } from 'react-native-elements'
+import { Icon } from 'react-native-elements'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { connect } from 'react-redux'
 import { StateToProps, DispatchToProps } from '../store/MapToProps'
-import { ActionMenu } from './components/Actions'
 import { PeriodUpdate } from './Dispatch/DispatchSend'
 
 export default Home = connect(
@@ -17,16 +16,6 @@ export default Home = connect(
 
   return (
     <SafeAreaView style={styles.Container}>
-      {/* <View style={styles.Header}>
-        <View style={styles.HeaderLeft}>
-          <View style={styles.HeaderIcon}>
-            <ActionMenu navigation={navigation} />
-          </View>
-          <Text style={styles.HeaderCaption}>{locale.project_name}</Text>
-        </View>
-        <View style={styles.HeaderRight}></View>
-      </View>
-      <Divider style={styles.Divider} /> */}
       <View style={styles.Home.Content}>
         <View style={styles.Home.Profiles}>
           <ProfileList
@@ -122,36 +111,6 @@ const ProfileList = ({ profiles, isDetails, styles, locale, navigation }) => {
                 {locale.home_profiles_empty_1}
               </Text>
             </View>
-            {/* <View style={styles.Empty.Section}>
-              <Text style={styles.Empty.Label}>
-                {locale.home_profiles_empty_2}
-              </Text>
-              <View style={styles.Empty.SectionRow}>
-                <Text style={styles.Empty.Label}>
-                  {locale.home_profiles_empty_3}&nbsp;&nbsp;
-                </Text>
-                <Icon
-                  name='user-plus'
-                  iconStyle={styles.Empty.Icon}
-                  type='font-awesome-5'
-                />
-                <Text style={styles.Empty.Label}>
-                  &nbsp;&nbsp;{locale.home_profiles_empty_4}
-                </Text>
-              </View>
-            </View>
-            <View style={styles.Empty.Section}>
-              <Text numberOfLines={5} style={styles.Empty.Label}>
-                {locale.home_profiles_empty_5}
-              </Text>
-              <TouchableOpacity
-                onPress={() => navigation.navigate('Instruction')}
-              >
-                <Text style={styles.Empty.Link}>
-                  {locale.home_profiles_empty_6}
-                </Text>
-              </TouchableOpacity>
-            </View> */}
           </View>
           <View style={styles.Empty.Filler}></View>
         </>
--- src/screens/Policy.js
+++ src/screens/Policy.js
@@ -1,10 +1,16 @@
-import React, { useCallback } from 'react';
-import { SafeAreaView, Text, View, Linking, TouchableOpacity } from 'react-native';
-import { Divider } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { StateToProps } from '../store/MapToProps';
-import { ActionBack } from './components/Actions';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useCallback } from 'react'
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Linking,
+  TouchableOpacity
+} from 'react-native'
+import { Divider } from 'react-native-elements'
+import { connect } from 'react-redux'
+import { StateToProps } from '../store/MapToProps'
+import { ActionBack } from './components/Actions'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const Policy = ({ navigation, locale, styles }) => {
   return (
@@ -19,82 +25,148 @@ const Policy = ({ navigation, locale, styles }) => {
       <Divider style={styles.Divider} /> */}
       <ScrollView style={styles.Policy.Container}>
         <View>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_company}</Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_company}
+          </Text>
           <Text style={styles.Policy.ParagraphText}>{locale.policy_city}</Text>
           <Text style={styles.Policy.ParagraphText}>{locale.policy_date}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_foreword}</Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_foreword}
+          </Text>
         </View>
         <View>
-          <Text style={styles.Policy.ParagraphHead}>{locale.policy_first_paragraph}</Text>
+          <Text style={styles.Policy.ParagraphHead}>
+            {locale.policy_first_paragraph}
+          </Text>
           <OpenUrlTouchableOpacity url={urlPolicy}>
-            <Text style={styles.Policy.ParagraphText}>{locale.policy_first_subparagraph1}</Text>
+            <Text style={styles.Policy.ParagraphText}>
+              {locale.policy_first_subparagraph1}
+            </Text>
           </OpenUrlTouchableOpacity>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_first_subparagraph2}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_first_subparagraph3}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_first_subparagraph4}</Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_first_subparagraph2}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_first_subparagraph3}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_first_subparagraph4}
+          </Text>
         </View>
         <View>
-          <Text style={styles.Policy.ParagraphHead}>{locale.policy_second_paragraph}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph1}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph1_1}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph1_2}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph2}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph3}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph4}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph5}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_second_subparagraph6}</Text>
+          <Text style={styles.Policy.ParagraphHead}>
+            {locale.policy_second_paragraph}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph1}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph1_1}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph1_2}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph2}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph3}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph4}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph5}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_second_subparagraph6}
+          </Text>
         </View>
         <View>
-          <Text style={styles.Policy.ParagraphHead}>{locale.policy_third_paragraph}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_third_subparagraph1}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_third_subparagraph2}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_third_subparagraph2_1}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_third_subparagraph2_2}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_third_subparagraph2_3}</Text>
+          <Text style={styles.Policy.ParagraphHead}>
+            {locale.policy_third_paragraph}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_third_subparagraph1}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_third_subparagraph2}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_third_subparagraph2_1}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_third_subparagraph2_2}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_third_subparagraph2_3}
+          </Text>
         </View>
         <View>
-          <Text style={styles.Policy.ParagraphHead}>{locale.policy_fourth_paragraph}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fourth_subparagraph1}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fourth_subparagraph2}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fourth_subparagraph3}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fourth_subparagraph3_1}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fourth_subparagraph3_2}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fourth_subparagraph3_3}</Text>
+          <Text style={styles.Policy.ParagraphHead}>
+            {locale.policy_fourth_paragraph}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fourth_subparagraph1}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fourth_subparagraph2}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fourth_subparagraph3}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fourth_subparagraph3_1}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fourth_subparagraph3_2}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fourth_subparagraph3_3}
+          </Text>
         </View>
         <View>
-          <Text style={styles.Policy.ParagraphHead}>{locale.policy_fifth_paragraph}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fifth_subparagraph1}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_fifth_subparagraph2}</Text>
+          <Text style={styles.Policy.ParagraphHead}>
+            {locale.policy_fifth_paragraph}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fifth_subparagraph1}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_fifth_subparagraph2}
+          </Text>
         </View>
 
         <View>
-          <Text style={styles.Policy.ParagraphHead}>{locale.policy_sixth_paragraph}</Text>
-          <Text style={styles.Policy.ParagraphText}>{locale.policy_sixth_subparagraph1}</Text>
+          <Text style={styles.Policy.ParagraphHead}>
+            {locale.policy_sixth_paragraph}
+          </Text>
+          <Text style={styles.Policy.ParagraphText}>
+            {locale.policy_sixth_subparagraph1}
+          </Text>
           <OpenUrlTouchableOpacity url={urlPolicy}>
-            <Text style={styles.Policy.ParagraphText}>{locale.policy_sixth_subparagraph2}</Text>
+            <Text style={styles.Policy.ParagraphText}>
+              {locale.policy_sixth_subparagraph2}
+            </Text>
           </OpenUrlTouchableOpacity>
-
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
-
 
 const urlPolicy = 'https://tau-quadra.com/privacy-policy'
 
 const OpenUrlTouchableOpacity = ({ url, children }) => {
   const handlePress = useCallback(async () => {
-    const supported = await Linking.canOpenURL(url);
+    const supported = await Linking.canOpenURL(url)
     if (supported) {
       await Linking.openURL(url)
     } else {
-      Alert.alert(`${locale.err_check_link} ${url}`);
+      Alert.alert(`${locale.err_check_link} ${url}`)
     }
-  }, [url]);
-  return <TouchableOpacity onPress={handlePress} >{children}</TouchableOpacity>
+  }, [url])
+  return <TouchableOpacity onPress={handlePress}>{children}</TouchableOpacity>
 }
 
-
-
-export default connect(StateToProps())(Policy);
+export default connect(StateToProps())(Policy)
