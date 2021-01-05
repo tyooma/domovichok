94f4a6a4bb40f5461d58d3e072285e57482e8fc5
maksym <mkopaiev@tau-quadra.com>
2021-01-05 17:46:20 +0200

    измененная текстовка инструкции(скрины еще не новые)

--- src/screens/Instruction.js
+++ src/screens/Instruction.js
@@ -1,60 +1,56 @@
-import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
-import { Icon, Divider, Image } from 'react-native-elements'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { ScrollView } from 'react-native-gesture-handler'
-import { connect } from 'react-redux'
-import { DispatchToProps, StateToProps } from '../store/MapToProps'
-import {
-  Collapse,
-  CollapseHeader,
-  CollapseBody
-} from 'accordion-collapse-react-native'
-import { ActionBack } from './components/Actions'
-import { PeriodUpdate } from './Dispatch/DispatchSend'
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import { Icon, Divider, Image } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
+import { connect } from 'react-redux';
+import { DispatchToProps, StateToProps } from '../store/MapToProps';
+import { Collapse, CollapseHeader, CollapseBody } from 'accordion-collapse-react-native';
+import { ActionBack } from './components/Actions';
+import { PeriodUpdate } from './Dispatch/DispatchSend';
 
 const Instruction = ({ locale, styles, media, toPeriod, navigation }) => {
   return (
     <SafeAreaView style={styles.Container}>
+      {/* <View style={styles.Header}>
+        <View style={styles.HeaderLeft}>
+          <View style={styles.HeaderIcon}><ActionBack navigation={navigation} /></View>
+          <Text style={styles.HeaderCaption}>{locale.instruction_caption}</Text>
+        </View>
+        <View style={styles.HeaderRight}>
+          <View style={styles.HeaderIconRight}>
+            <TouchableOpacity onPress={() => PeriodUpdate(locale, toPeriod)}>
+              <Icon name="sync-alt" iconStyle={styles.Help.PeriodUpdate} type="font-awesome-5" />
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.HeaderCaption}></Text>
+        </View>
+      </View>
+      <Divider style={styles.Divider} /> */}
       <View style={styles.Help.Container}>
-        <ScrollView>
+      <ScrollView>
+          {/* <Divider style={styles.Divider} /> */}
           <Collapse>
             <CollapseHeader>
               <View style={styles.Help.slideContainer}>
-                <Text style={styles.Help.slideTitle}>
-                  {locale.help_main_page}
-                </Text>
+                <Text style={styles.Help.slideTitle}>{locale.help_main_page}</Text>
               </View>
             </CollapseHeader>
             <CollapseBody>
               <View>
                 <View style={styles.Help.imageContainer}>
-                  <Image
-                    style={styles.Help.imageContent}
-                    source={media.Help.HomeScreenEmpty}
-                  />
+                  <Image style={styles.Help.imageContent} source={media.Help.HomeScreenEmpty} />
                 </View>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_main_page_text_1}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_main_page_text_2}
-                </Text>
-                <Text style={styles.Help.lastRegularText}>
-                  {locale.help_main_page_text_3}
-                </Text>
+                <Text style={styles.Help.regularText}>{locale.help_main_page_text_1}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_main_page_text_2}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_main_page_text_3}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_main_page_text_4}</Text>
+                <Text style={styles.Help.lastRegularText}>{locale.help_main_page_text_5}</Text>
                 <View style={styles.Help.imageContainer}>
-                  <Image
-                    style={styles.Help.imageContent}
-                    source={media.Help.HomeScreenFull}
-                  />
+                  <Image style={styles.Help.imageContent} source={media.Help.HomeScreenFull} />
                 </View>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_main_page_text_4}
-                </Text>
-                <Text style={styles.Help.lastRegularText}>
-                  {locale.help_main_page_text_5}
-                </Text>
+                <Text style={styles.Help.regularText}>{locale.help_main_page_text_6}</Text>
+                <Text style={styles.Help.lastRegularText}>{locale.help_main_page_text_7}</Text>
               </View>
             </CollapseBody>
           </Collapse>
@@ -62,46 +58,25 @@ const Instruction = ({ locale, styles, media, toPeriod, navigation }) => {
           <Collapse>
             <CollapseHeader>
               <View style={styles.Help.slideContainer}>
-                <Text style={styles.Help.slideTitle}>
-                  {locale.help_profile_page}
-                </Text>
+                <Text style={styles.Help.slideTitle}>{locale.help_profile_page}</Text>
               </View>
             </CollapseHeader>
             <CollapseBody>
               <View>
                 <View style={styles.Help.imageContainer}>
-                  <Image
-                    style={styles.Help.imageContent}
-                    source={media.Help.Profile}
-                  />
+                  <Image style={styles.Help.imageContent} source={media.Help.Profile} />
                 </View>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_foreword}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_paragraph}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_paragraph_1_1}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_paragraph_1_2}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_paragraph_2}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_paragraph_3}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_paragraph_4}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_profile_heatmeter_details}
-                </Text>
-                <Text style={styles.Help.lastRegularText}>
-                  {locale.help_profile_save}
-                </Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_foreword}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_edit}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_delete}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_paragraph}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_paragraph_1_1}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_paragraph_1_2}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_paragraph_2}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_paragraph_3}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_paragraph_4}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_profile_heatmeter_details}</Text>
+                <Text style={styles.Help.lastRegularText}>{locale.help_profile_save}</Text>
               </View>
             </CollapseBody>
           </Collapse>
@@ -109,31 +84,18 @@ const Instruction = ({ locale, styles, media, toPeriod, navigation }) => {
           <Collapse>
             <CollapseHeader>
               <View style={styles.Help.slideContainer}>
-                <Text style={styles.Help.slideTitle}>
-                  {locale.help_send_page}
-                </Text>
+                <Text style={styles.Help.slideTitle}>{locale.help_send_page}</Text>
               </View>
             </CollapseHeader>
             <CollapseBody>
               <View>
                 <View style={styles.Help.imageContainer}>
-                  <Image
-                    style={styles.Help.imageContent}
-                    source={media.Help.SendDataEmpty}
-                  />
+                  <Image style={styles.Help.imageContent} source={media.Help.SendDataEmpty} />
                 </View>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_send_foreword}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_send_text_1}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_send_text_2}
-                </Text>
-                <Text style={styles.Help.lastRegularText}>
-                  {locale.help_send_text_3}
-                </Text>
+                <Text style={styles.Help.regularText}>{locale.help_send_foreword}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_send_text_1}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_send_text_2}</Text>
+                <Text style={styles.Help.lastRegularText}>{locale.help_send_text_3}</Text>
               </View>
             </CollapseBody>
           </Collapse>
@@ -141,34 +103,23 @@ const Instruction = ({ locale, styles, media, toPeriod, navigation }) => {
           <Collapse>
             <CollapseHeader>
               <View style={styles.Help.slideContainer}>
-                <Text style={styles.Help.slideTitle}>
-                  {locale.help_history_page}
-                </Text>
+                <Text style={styles.Help.slideTitle}>{locale.help_history_page}</Text>
               </View>
             </CollapseHeader>
             <CollapseBody>
               <View>
                 <View style={styles.Help.imageContainer}>
-                  <Image
-                    style={styles.Help.imageContent}
-                    source={media.Help.HistoryScreen}
-                  />
+                  <Image style={styles.Help.imageContent} source={media.Help.HistoryScreen} />
                 </View>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_history_foreword}
-                </Text>
-                <Text style={styles.Help.regularText}>
-                  {locale.help_history_condition}
-                </Text>
+                <Text style={styles.Help.regularText}>{locale.help_history_foreword}</Text>
+                <Text style={styles.Help.regularText}>{locale.help_history_condition}</Text>
 
-                <Text style={styles.Help.lastRegularText}>
-                  {locale.help_history_delete}
-                </Text>
+                <Text style={styles.Help.lastRegularText}>{locale.help_history_delete}</Text>
               </View>
             </CollapseBody>
           </Collapse>
           <Divider style={styles.Divider} />
-          <Collapse>
+          {/* <Collapse>
             <CollapseHeader>
               <View style={styles.Help.slideContainer}>
                 <Text style={styles.Help.slideTitle}>{locale.help_period}</Text>
@@ -176,17 +127,15 @@ const Instruction = ({ locale, styles, media, toPeriod, navigation }) => {
             </CollapseHeader>
             <CollapseBody>
               <View>
-                <Text style={styles.Help.lastRegularText}>
-                  {locale.help_show_period}
-                </Text>
+                <Text style={styles.Help.lastRegularText}>{locale.help_show_period}</Text>
               </View>
             </CollapseBody>
           </Collapse>
-          <Divider style={styles.Divider} />
+          <Divider style={styles.Divider} /> */}
         </ScrollView>
       </View>
     </SafeAreaView>
-  )
+  );
 }
 
-export default connect(StateToProps(), DispatchToProps())(Instruction)
+export default connect(StateToProps(), DispatchToProps())(Instruction);
